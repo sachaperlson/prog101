d10baf7011220718cd9ed0d95858622d23eab7a3
sachaperlson <sacha@ucsb.edu>
2025-01-19 15:10:28 -0800

    complete PROG101

--- exercises/prog101-using-functions.R
+++ exercises/prog101-using-functions.R
@@ -1,13 +1,29 @@
 # Fill in the blanks so the answers are all 2. Use help for unfamiliar functions
 # if necessary.
-log10(????)
-????(4)
-ceiling(????.1)
+log10(100)
+
+.5*(4)
+ceiling(1.1)
+
+?ceiling
+always give you the upper number when rounding
 
 # Find the functions for cosine and sine. What are the cosine and sine of 0?
+
+cos(0)
+#1
+
+sin(0)
+#0
+
+sin(pi)
+#0
 
 # What do you think the following functions do? Just go off the name, don't look
 # them up.
 read.csv()
+#reads a csv file (like google sheets)
 boxplot()
+#I think this function creates a box plot with a set of values
 startsWith()
+#I think this can organize a chart and put it in order
